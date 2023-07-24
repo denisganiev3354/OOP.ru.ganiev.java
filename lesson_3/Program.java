39e9a9380d6da109322f144cf3838c84c15142f6
denisganiev3354 <denisganiev3354@gmail.com>
2023-07-24 21:40:19 +0400

    remove

--- lesson_3/Program.java
+++
@@ -1,7 +0,0 @@
-package ru.ganiev.OOP.lesson_3;
-
-public class Program {
-    public static void main(String[] args) {
-
-    }
-}
--- lesson_3/StudentsGroupIterator.java
+++
@@ -1,9 +0,0 @@
-package ru.ganiev.OOP.lesson_3;
-
-import java.util.*;
-//public class StudentsGroupIterator implements Iterator<Student> {
-//    private List<Student> students = new ArrayList<Student>();
-//    public StudentsGroupIterator()
-//
-
-//}
